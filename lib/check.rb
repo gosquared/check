6a4c2a4360b3033a059279a5ec28336ab5c56e78
Gerhard Lazu <gerhard@lazu.co.uk>
2012-11-09 01:00:30 +0000

    Better fetch defaults

--- lib/check.rb
+++ lib/check.rb
@@ -5,9 +5,9 @@ module Check
   extend self
 
   # Can be either redis:// or unix://
-  REDIS_URI = ENV.fetch('REDIS_URI') { "redis://localhost:6379" }
-  REDIS_DB  = ENV.fetch('REDIS_DB') { 0 }.to_i
-  REDIS_NOTIFICATIONS = ENV.fetch('REDIS_NOTIFICATIONS') { "check_notifications" }
+  REDIS_URI = ENV.fetch('REDIS_URI', "redis://localhost:6379")
+  REDIS_DB  = ENV.fetch('REDIS_DB', 0).to_i
+  REDIS_NOTIFICATIONS = ENV.fetch('REDIS_NOTIFICATIONS', "check_notifications")
 
   Redis.current = Redis.new(
     url:     Check::REDIS_URI,
